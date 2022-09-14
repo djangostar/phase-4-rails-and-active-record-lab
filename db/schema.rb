69f838d22f37738f70d3124aef342633f254dc19
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-09-14 13:25:02 -0700

    commit tree 912394bfe4174efe012bfe1ef1c1bbd7f9f09940

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_054435) do
+ActiveRecord::Schema.define(version: 2022_09_14_200716) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
